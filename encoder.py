f42c888540e2078dfc231320c0422e686d5da8a9
Sidi Liang <1467329765@qq.com>
2024-02-04 23:56:59 +0800

    Implemented Reed-Solomon FEC

--- encoder.py
+++ encoder.py
@@ -4,6 +4,7 @@ Liang Sidi, 2024
 import wave
 import gzip
 import numpy as np
+from reedsolo import RSCodec
 
 class Modulator:
     """Class for modulating text to carrier audio signals.
@@ -16,6 +17,12 @@ class Modulator:
                               Currently only supports BPSK (1), MFSK (2).
         m_for_mfsk(int): The M in MFSK, default to 64-FSK. Must be a power of 2.
         compression_enabled(bool): Whether to compress the binary data using GZIP.
+        start_chrip_seq(list): Chrip signal frequencies for preamble.
+        end_chrip_seq(list): Chrip signal frequencies for postamble.
+        preamble_duration(float): The duration of the preamble in seconds.
+        postamble_duration(float): The duration of the postamble in seconds.
+        fec_enabled(bool): Whether to enable Reed-Solomon forward error correction.
+        
         
     Methods:
         __init__(self, 
@@ -25,6 +32,8 @@ class Modulator:
                 bandwidth=4400,
                 modulation_mode=2,
                 m_for_mfsk=64,
+                fec_enabled=True,
+                fec_nsym=10,
                 compression_enabled=True): Initializes the modulator.
         generate_signal(self, text): Generates the acoustic signal for the given text.
         __string_to_binary(self, text): Converts text to binary data.
@@ -37,7 +46,8 @@ class Modulator:
     """
     def __init__(self, carrier_freq=8800, sample_rate=44100,
                  bit_duration=0.01, bandwidth=4400, modulation_mode=2,
-                 m_for_mfsk=64, compression_enabled=True):
+                 m_for_mfsk=64, fec_enabled=True, fec_nsym=50,
+                 compression_enabled=True):
         """Initializes the modulator.
         Parameters:
             carrier_freq(int, default=8800): The frequency of the carrier signal in Hz.
@@ -47,6 +57,9 @@ class Modulator:
             modulation_mode(int, default=2): The modulation mode.
                                              Currently only supports BPSK (1), MFSK (2).
             m_for_mfsk(int, default=64): The M in MFSK, default to 64-FSK. Must be a power of 2.
+            fec_enabled(bool, default=True): Whether to enable Reed-Solomon 
+                                             forward error correction.
+            fec_nsym(int, default=10): The number of symbols for Reed-Solomon FEC.
             compression_enabled(bool, default=True): Whether to compress the binary data using GZIP.
         """
         self.carrier_freq = carrier_freq
@@ -65,25 +78,50 @@ class Modulator:
         self.end_chrip_seq = self.start_chrip_seq[::-1]
         self.preamble_duration = 1
         self.postamble_duration = 1
+        self.fec_enabled = fec_enabled
+        self.fec_nsym = fec_nsym
+        if fec_enabled:
+            self.rscodec = RSCodec(self.fec_nsym)
 
     def generate_signal(self, text):
         """Generates the signal from text.
-        Protocol: Chirp + 0.5s + "HMMSG##<text>" + 0.5s + Chirp + 1s
+        Protocol: Chrip + 0.5s + H# + Data + ## + Chrip + 0.5s
         Parameters:
             text(str): The text to be modulated.
         Returns:
             The signal.
         """
+        print("Hearme Encoder Module. Liang Sidi, 2024.")
+        print("Generating signal...")
+        # Print Parameters
+        print(f"Carrier Frequency: {self.carrier_freq}")
+        print(f"Sample Rate: {self.sample_rate}")
+        print(f"Bit Duration: {self.bit_duration}")
+        print(f"Bandwidth: {self.bandwidth}")
+        print(f"Modulation Mode: {self.modulation_mode}")
+        print(f"M for MFSK: {self.m_for_mfsk}")
+        print(f"Compression Enabled: {self.compression_enabled}")
+        print(f"FEC Enabled: {self.fec_enabled}")
+        print(f"FEC Nsym: {self.fec_nsym}")
+        
         stepped_chirp_start = self.generate_stepped_chirp(self.preamble_duration,
                                                           self.start_chrip_seq)
         stepped_chirp_end = self.generate_stepped_chirp(self.postamble_duration,
                                                         self.end_chrip_seq)
-        text = f"HMMSG##{text}"
-        binary_data = self.__string_to_binary(text, gzip_enabled=self.compression_enabled)
-        print(binary_data)
+        binary_data = self.__string_to_binary(text,
+                                              gzip_enabled=self.compression_enabled,
+                                              fec_enabled=self.fec_enabled)
+        # print(binary_data)
+        msg_header = "H#" # Header for the message, len=16 bits
+        msg_end = "##" # End of message, len=16 bits
+        msg_header_binary = ''.join(format(ord(char), '08b') for char in msg_header)
+        msg_end_binary = ''.join(format(ord(char), '08b') for char in msg_end)
+        binary_data = msg_header_binary + binary_data + msg_end_binary
+        print(f"Sent data length: {len(binary_data)}")
+        print(f"Sent data: {binary_data}")
         _, modulated_signal = self.__modulate(binary_data)
         signal = np.concatenate((stepped_chirp_start, np.zeros(int(self.sample_rate * 0.5)),
-                                    modulated_signal, np.zeros(int(self.sample_rate * 0.5)),
+                                    modulated_signal, np.zeros(int(self.sample_rate * self.bit_duration)),
                                     stepped_chirp_end, np.zeros(self.sample_rate)))
         return signal
 
@@ -122,21 +160,33 @@ class Modulator:
 
         return chirp_signal
 
-    def __string_to_binary(self, text, gzip_enabled=True):
+    def __string_to_binary(self, text, gzip_enabled=True, fec_enabled=True):
         """Converts text to binary data.
         Parameters:
             text(str): The text to be converted.
             gzip_enabled(bool, default=True): Whether to compress the binary data using GZIP.
+            fec_enabled(bool, default=True): Whether to apply Reed-Solomon forward error correction.
         Returns:
             The binary data.
         """
-        binary_data = ''.join(format(byte, '08b') for byte in text.encode('utf-8'))
-        # print(binary_data)
+        data_bytes = text.encode('utf-8')
+        binary_data = ''.join(format(byte, '08b') for byte in data_bytes)
+        print("Raw msg: " + binary_data)
+        print("___________________________________________________________________________")
+        raw_msg_len = len(binary_data)
+        print(f"Raw msg len: {raw_msg_len}")
         if gzip_enabled:
-            data_bytes = int(binary_data, 2).to_bytes((len(binary_data) + 7) // 8, byteorder='big')
-            compressed_data = gzip.compress(data_bytes)
-            binary_data = ''.join(format(byte, '08b') for byte in compressed_data)
-            # print(binary_data)
+            # Compress the binary data using GZIP
+            data_bytes = gzip.compress(data_bytes)
+            compressed_msg_len = len(data_bytes)*8 # in bits
+            print(f"Compressed msg len: {compressed_msg_len}")
+        if fec_enabled:
+            # Apply Reed-Solomon forward error correction
+            data_bytes = self.rscodec.encode(data_bytes)
+        binary_data = ''.join(format(byte, '08b') for byte in data_bytes)
+        encoded_len = len(binary_data)
+        print(f"Encoded msg len: {encoded_len}")
+        print("Encoded msg: " + binary_data)
         return binary_data
 
     def __mfsk_modulate(self, binary_data, M=64):
@@ -217,7 +267,7 @@ class Modulator:
             return self.__mfsk_modulate(binary_data, M=self.m_for_mfsk)
         return None
 
-    def modulate(self, text, mode=2, compression_enabled=True):
+    def modulate(self, text, mode=2, compression_enabled=True, fec_enabled=True):
         """Modulates text to carrier audio signal.
         Parameters: 
             text(str): The text to be modulated.
@@ -227,7 +277,9 @@ class Modulator:
         Returns:
             The modulated signal.
         """
-        binary_data = self.__string_to_binary(text, gzip_enabled=compression_enabled)
+        binary_data = self.__string_to_binary(text,
+                                              gzip_enabled=compression_enabled,
+                                              fec_enabled=fec_enabled)
         if mode == 1:
             return self.__bpsk_modulate(binary_data)
         elif mode == 2:
@@ -262,36 +314,39 @@ class Modulator:
 
         print(f"File saved as {filename}")
 
-    
+
 
 # Example usage
 modulator = Modulator()
 modulator.compression_enabled = False
-modulator.bit_duration = 0.1
+modulator.carrier_freq = 16000
+modulator.sample_rate = 44100
+modulator.bit_duration = 0.05
 # text_to_encode = "The quick brown fox jumps over the lazy dog."
-TEXT_TO_ENCODE = "The quick brown fox jumps over the lazy dog. \n\
-                  天地玄黄，宇宙洪荒，日月盈仄，辰宿列张，\n\
-                  寒来暑往，秋收冬藏，闰余成岁，律吕调阳。"
+# TEXT_TO_ENCODE = "The quick brown fox jumps over the lazy dog. \n\
+#                   天地玄黄，宇宙洪荒，日月盈仄，辰宿列张，\n\
+#                   寒来暑往，秋收冬藏，闰余成岁，律吕调阳。"
+TEXT_TO_ENCODE = "Hello, World!"
 generated_signal = modulator.generate_signal(TEXT_TO_ENCODE)
 modulator.save_to_wav(generated_signal, "hello_world_stream_test_64fsk.wav",
                       add_gaussian_noise=False)
 
-# For testing
-_, signal = modulator.modulate(TEXT_TO_ENCODE, mode=2, compression_enabled=False)
-# play the signal
-# import sounddevice as sd
-# sd.play(signal, modulator.sample_rate, blocking=True)
+# # For testing
+# _, signal = modulator.modulate(TEXT_TO_ENCODE, mode=2, compression_enabled=False)
+# # play the signal
+# # import sounddevice as sd
+# # sd.play(signal, modulator.sample_rate, blocking=True)
 
-modulator.save_to_wav(signal, "hello_world_64fsk.wav", add_gaussian_noise=False)
-modulator.save_to_wav(signal, "hello_world_64fsk_gaussian.wav", add_gaussian_noise=True)
-_, signal_gz = modulator.modulate(TEXT_TO_ENCODE, mode=2, compression_enabled=True)
-modulator.save_to_wav(signal_gz, "hello_world_64fsk_gzip.wav", add_gaussian_noise=False)
-modulator.save_to_wav(signal_gz, "hello_world_64fsk_gaussian_gzip.wav", add_gaussian_noise=True)
+# modulator.save_to_wav(signal, "hello_world_64fsk.wav", add_gaussian_noise=False)
+# modulator.save_to_wav(signal, "hello_world_64fsk_gaussian.wav", add_gaussian_noise=True)
+# _, signal_gz = modulator.modulate(TEXT_TO_ENCODE, mode=2, compression_enabled=True)
+# modulator.save_to_wav(signal_gz, "hello_world_64fsk_gzip.wav", add_gaussian_noise=False)
+# modulator.save_to_wav(signal_gz, "hello_world_64fsk_gaussian_gzip.wav", add_gaussian_noise=True)
 
 
-_, signal_bpsk = modulator.modulate(TEXT_TO_ENCODE, mode=1, compression_enabled=False)
-modulator.save_to_wav(signal_bpsk, "hello_world_bpsk.wav")
-modulator.save_to_wav(signal_bpsk, "hello_world_bpsk_gaussian.wav", add_gaussian_noise=True)
-_, signal_bpsk_gz = modulator.modulate(TEXT_TO_ENCODE, mode=1, compression_enabled=True)
-modulator.save_to_wav(signal_bpsk_gz, "hello_world_bpsk_gzip.wav", add_gaussian_noise=False)
-modulator.save_to_wav(signal_bpsk_gz, "hello_world_bpsk_gaussian_gzip.wav", add_gaussian_noise=True)
+# _, signal_bpsk = modulator.modulate(TEXT_TO_ENCODE, mode=1, compression_enabled=False)
+# modulator.save_to_wav(signal_bpsk, "hello_world_bpsk.wav")
+# modulator.save_to_wav(signal_bpsk, "hello_world_bpsk_gaussian.wav", add_gaussian_noise=True)
+# _, signal_bpsk_gz = modulator.modulate(TEXT_TO_ENCODE, mode=1, compression_enabled=True)
+# modulator.save_to_wav(signal_bpsk_gz, "hello_world_bpsk_gzip.wav", add_gaussian_noise=False)
+# modulator.save_to_wav(signal_bpsk_gz, "hello_world_bpsk_gaussian_gzip.wav", add_gaussian_noise=True)
